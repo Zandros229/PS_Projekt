953227ab813ebe2e02047906c338fbb73d3f5ff8
Zandros229 <ernest.figura548@gmail.com>
2019-06-03 12:54:25 +0200

    Initial commit

---
+++ src/Server/MulticastServer.java
@@ -0,0 +1,33 @@
+package Server;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public class MulticastServer extends Thread {
+    protected MulticastSocket socket = null;
+    protected byte[] buf = new byte[256];
+
+    public void run() {
+        try {
+            socket = new MulticastSocket(4446);
+            InetAddress group = InetAddress.getByName("230.0.0.0");
+            socket.joinGroup(group);
+            while (true) {
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                socket.receive(packet);
+                String received = new String(
+                        packet.getData(), 0, packet.getLength());
+                if ("end".equals(received)) {
+                    break;
+                }
+            }
+
+            socket.leaveGroup(group);
+        } catch (IOException e) {
+            System.out.printf(e.getMessage());
+        }
+        socket.close();
+    }
+}
